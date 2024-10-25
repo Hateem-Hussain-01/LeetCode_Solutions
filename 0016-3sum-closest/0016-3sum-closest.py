04389ae581651758dd5898da6f097f3e8cd4fbca
Muhammad Hateem Hussain <mhateemjutt1412@gmail.com>
2024-10-25 20:52:20 +0500

    Time: 363 ms (78.38%), Space: 16.6 MB (85.33%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,26 +1,45 @@
+# class Solution:
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+#         nums.sort()
+#         diff=float('inf')
+#         for i in range(len(nums)-2):
+#             left=i+1
+#             right=len(nums)-1
+#             while left<right:
+#                 sum1=nums[i]+nums[left]+nums[right]
+#                 if sum1 ==target:
+#                     return target
+#                 elif abs(target-sum1) < diff:
+#                     diff=abs(target-sum1)
+#                     ans=sum1
+#                 elif sum1> target:
+#                     right-=1
+#                 else:
+#                     left+=1        
+#         return ans
+
+
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+
         nums.sort()
-        diff=float('inf')
+        res = nums[0] + nums[1] + nums[2]
+                
         for i in range(len(nums)-2):
-            left=i+1
-            right=len(nums)-1
-            while left<right:
-                sum=nums[i]+nums[left]+nums[right]
-                if sum ==target:
-                    return target
-                elif abs(target-sum) < diff:
-                    diff=abs(target-sum)
-                    ans=sum
-                elif sum> target:
-                    right-=1
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == target:
+                    return s
+                if abs(s - target) < abs(res - target):
+                    res = s
+                if s < target:
+                    l += 1
                 else:
-                    left+=1        
-        return ans
+                    r -= 1
+                            
+        return res
 
 
 
-
-
-
-        
